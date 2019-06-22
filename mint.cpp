3cf86e5c5605e026362eaf08c42a6fa5e797f327
atcoder-live <atcoder.live@gmail.com>
2019-06-23 01:45:11 +0900

    initial commit

---
+++ mint.cpp
@@ -0,0 +1,31 @@
+// https://youtu.be/L8grWxBlIZ4?t=9858
+// https://youtu.be/ERZuLAxZffQ?t=4765
+const int mod = 1000000007;
+struct mint {
+  ll x;
+  mint(ll x=0):x(x%mod){}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod-a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) {
+    (x *= a.x) %= mod;
+    return *this;
+  }
+  mint operator+(const mint a) const {
+    mint res(*this);
+    return res+=a;
+  }
+  mint operator-(const mint a) const {
+    mint res(*this);
+    return res-=a;
+  }
+  mint operator*(const mint a) const {
+    mint res(*this);
+    return res*=a;
+  }
+};
