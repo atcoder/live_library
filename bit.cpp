fe4955aef93c5c523e464e45f2030e22d2c10d80
atcoder-live <atcoder.live@gmail.com>
2019-08-05 03:24:17 +0900

    BIT

---
+++ bit.cpp
@@ -0,0 +1,20 @@
+// Binary Indexed Tree (Fenwick Tree)
+// https://youtu.be/lyHk98daDJo?t=7960
+template<typename T>
+struct BIT {
+  int n;
+  vector<T> d;
+  BIT(int n=0):n(n),d(n+1) {}
+  void add(int i, T x=1) {
+    for (i++; i <= n; i += i&-i) {
+      d[i] += x;
+    }
+  }
+  T sum(int i) {
+    T x = 0;
+    for (i++; i; i -= i&-i) {
+      x += d[i];
+    }
+    return x;
+  }
+};
