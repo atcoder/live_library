a28cbf79bc9dc533799b1101ec2062a868e1d251
atcoder-live <atcoder.live@gmail.com>
2019-09-02 04:40:17 +0900

    geometry - Vector

---
+++ geom.cpp
@@ -0,0 +1,36 @@
+// geometry
+// https://youtu.be/UWbGRhF3Ozw?t=9564
+const double eps = 1e-9;
+struct V {
+  double x, y;
+  V(double x=0, double y=0): x(x), y(y) {}
+  V& operator+=(const V& v) { x += v.x; y += v.y; return *this;}
+  V operator+(const V& v) const { return V(*this) += v;}
+  V& operator-=(const V& v) { x -= v.x; y -= v.y; return *this;}
+  V operator-(const V& v) const { return V(*this) -= v;}
+  V& operator*=(double s) { x *= s; y *= s; return *this;}
+  V operator*(double s) const { return V(*this) *= s;}
+  V& operator/=(double s) { x /= s; y /= s; return *this;}
+  V operator/(double s) const { return V(*this) /= s;}
+  double dot(const V& v) const { return x*v.x + y*v.y;}
+  double cross(const V& v) const { return x*v.y - v.x*y;}
+  double norm2() const { return x*x + y*y;}
+  double norm() const { return sqrt(norm2());}
+  
+  int ort() const { // orthant
+    if (abs(x) < eps && abs(y) < eps) return 0;
+    if (y > 0) return x>0 ? 1 : 2;
+    else return x>0 ? 4 : 3;
+  }
+  bool operator<(const V& v) const {
+    int o = ort(), vo = v.ort();
+    if (o != vo) return o < vo;
+    return cross(v) > 0;
+  }
+};
+istream& operator>>(istream& is, V& v) {
+  is >> v.x >> v.y; return is;
+}
+ostream& operator<<(ostream& os, const V& v) {
+  os<<"("<<v.x<<","<<v.y<<")"; return os;
+}
