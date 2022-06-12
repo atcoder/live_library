17d68ef2a4f9e0180a518e575de09f4b77416261
snuke <snukent@gmail.com>
2022-06-13 01:31:58 +0900

    coodinate compression

---
+++ cc.cpp
@@ -0,0 +1,26 @@
+// Coodinate Compression
+// https://youtu.be/fR3W5IcBGLQ?t=8550
+template<typename T=int>
+struct CC {
+  bool initialized;
+  vector<T> xs;
+  CC(): initialized(false) {}
+  void add(T x) { xs.push_back(x);}
+  void init() {
+    sort(xs.begin(), xs.end());
+    xs.erase(unique(xs.begin(),xs.end()),xs.end());
+    initialized = true;
+  }
+  int operator()(T x) {
+    if (!initialized) init();
+    return upper_bound(xs.begin(), xs.end(), x) - xs.begin() - 1;
+  }
+  T operator[](int i) {
+    if (!initialized) init();
+    return xs[i];
+  }
+  int size() {
+    if (!initialized) init();
+    return xs.size();
+  }
+};
