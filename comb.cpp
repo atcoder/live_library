7292cf24a67a6cd007368bb625812ab6b7c30582
snuke <snukent@gmail.com>
2022-09-18 02:23:46 +0900

    update comb.cpp

--- comb.cpp
+++ comb.cpp
@@ -1,17 +1,34 @@
-[mint]
 // combination mod prime
-// https://www.youtube.com/watch?v=8uowVvQ_-Mo&feature=youtu.be&t=1619
-struct combination {
-  vector<mint> fact, ifact;
-  combination(int n):fact(n+1),ifact(n+1) {
-    assert(n < mod);
-    fact[0] = 1;
-    for (int i = 1; i <= n; ++i) fact[i] = fact[i-1]*i;
-    ifact[n] = fact[n].inv();
-    for (int i = n; i >= 1; --i) ifact[i-1] = ifact[i]*i;
+// https://youtu.be/8uowVvQ_-Mo?t=6002
+// https://youtu.be/Tgd_zLfRZOQ?t=9928
+struct modinv {
+  int n; vector<mint> d;
+  modinv(): n(2), d({0,1}) {}
+  mint operator()(int i) {
+    while (n <= i) d.push_back(-d[mint::mod%n]*(mint::mod/n)), ++n;
+    return d[i];
   }
-  mint operator()(int n, int k) {
-    if (k < 0 || k > n) return 0;
-    return fact[n]*ifact[k]*ifact[n-k];
+  mint operator[](int i) const { return d[i];}
+} invs;
+struct modfact {
+  int n; vector<mint> d;
+  modfact(): n(2), d({1,1}) {}
+  mint operator()(int i) {
+    while (n <= i) d.push_back(d.back()*n), ++n;
+    return d[i];
   }
-};
+  mint operator[](int i) const { return d[i];}
+} facts;
+struct modfactinv {
+  int n; vector<mint> d;
+  modfactinv(): n(2), d({1,1}) {}
+  mint operator()(int i) {
+    while (n <= i) d.push_back(d.back()*invs(n)), ++n;
+    return d[i];
+  }
+  mint operator[](int i) const { return d[i];}
+} ifacts;
+mint comb(int n, int k) {
+  if (n < k || k < 0) return 0;
+  return facts(n)*ifacts(k)*ifacts(n-k);
+}
