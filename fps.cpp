536f7a7285dbd286197ecb0fb0b2d4a82b351d3e
snuke <snukent@gmail.com>
2023-02-16 00:32:41 +0900

    add fps.inv

--- fps.cpp
+++ fps.cpp
@@ -6,6 +6,8 @@ struct fps : vm {
   template<class...Args> fps(Args...args): vm(args...) {}
   fps(initializer_list<mint> a): vm(a.begin(),a.end()) {}
   void rsz(int n) { if (s < n) resize(n);}
+  fps& low_(int n) { resize(n); return d;}
+  fps low(int n) const { return fps(d).low_(n);}
   mint& operator[](int i) { rsz(i+1); return vm::operator[](i);}
   mint operator[](int i) const { return i<s ? vm::operator[](i) : 0;}
   mint operator()(mint x) const {
@@ -17,13 +19,20 @@ struct fps : vm {
   fps& operator+=(const fps& a) { rsz(a.size()); rep(i,a.size()) d[i] += a[i]; return d;}
   fps& operator-=(const fps& a) { rsz(a.size()); rep(i,a.size()) d[i] -= a[i]; return d;}
   fps& operator*=(const fps& a) { return d = convolution(d, a);}
-  fps& operator*=(mint a) { rep(i,s) d[i] *= a; return *this;}
-  fps& operator/=(mint a) { rep(i,s) d[i] /= a; return *this;}
+  fps& operator*=(mint a) { rep(i,s) d[i] *= a; return d;}
+  fps& operator/=(mint a) { rep(i,s) d[i] /= a; return d;}
   fps operator+(const fps& a) const { return fps(d) += a;}
   fps operator-(const fps& a) const { return fps(d) -= a;}
   fps operator*(const fps& a) const { return fps(d) *= a;}
   fps operator*(mint a) const { return fps(d) *= a;}
   fps operator/(mint a) const { return fps(d) /= a;}
+  fps operator~() const {
+    fps r({d[0].inv()});
+    for (int i = 1; i < s; i <<= 1) r = r*mint(2) - (r*r*low(i<<1)).low(i<<1);
+    return r.low_(s);
+  }
+  fps& operator/=(const fps& a) { int w = s; d *= ~a; return d.low_(w);}
+  fps operator/(const fps& a) const { return fps(d) /= a;}
   fps integ() const {
     fps r;
     rep(i,s) r[i+1] = d[i]/(i+1);
